a841246f403828303107d011417ff50299aeea49
jonathan alexis castañeda aviles <jonathanalexiscastanedaaviles@gmail.com>
2024-12-13 08:58:36 -0700

    primer commit

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,34 @@
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive,
+     IsString, MinLength 
+    } from "class-validator";
+
+export class CreateProductDto {
+    @IsString()
+    @MinLength(1)
+    title:string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?:number;
+
+    @IsString()
+    @IsOptional()
+    desciption?:string;
+
+    @IsString()
+    @IsOptional()
+    slug?:string;
+
+    @IsInt()
+    @IsPositive()
+    @IsOptional()
+    stock?:number;
+
+    @IsString({each:true})
+    @IsArray()
+    size:string[];
+
+    @IsIn(['men','woman','kids','unisex'])
+    gender:string
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,48 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+
+
+@Injectable()
+export class ProductsService {
+  private readonly  logger = new Logger('Product Service');
+  constructor(
+    @InjectRepository(Product)
+    private readonly  ProductRepository:Repository<Product>
+  ){}
+  async create(createProductDto: CreateProductDto) {
+    try {
+       const product =  this.ProductRepository.create(createProductDto);
+       await  this.ProductRepository.save(product)
+       return product;
+    } catch (error) {
+     this.handleDbExeptions(error);
+    }
+  }
+
+  findAll() {
+    return `This action returns all products`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+  handleDbExeptions (error:any){
+    if(error.code === '23505'){
+      throw new BadRequestException(error.detail)
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException('check server logs')
+  }
+}
